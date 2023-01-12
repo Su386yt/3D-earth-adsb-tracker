a513dd78c25a7a0cb6aa02d7a1665aab62e8df20
Su386yt <83100266+Su386yt@users.noreply.github.com>
2023-01-12 08:25:55 -0800

    Revert "Created PlaneBehavior and PlaneData scripts"
    This reverts commit 900e15b273e85fd958dc27fe2187240ae6cd200b.

--- Assets/Scripts/Gamemanager.cs
+++ Assets/Scripts/Gamemanager.cs
@@ -1,6 +1,5 @@
 using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
-using Newtonsoft.Json.Linq;
 using SimpleJSON;
 using System;
 using System.Collections;
@@ -14,33 +13,25 @@ using UnityEngine.UIElements;
 public class Gamemanager : MonoBehaviour
 {
     public TextAsset textAsset;
+    public string jsonText;
+    public string username;
+    public string password;
+    public long lastRequestTime = 0;
+    public long lastPlanePlaceTime = 0;
+    public List<GameObject> planeObjects;
     public GameObject earthPrefab;
     public GameObject earth;
     public SpinFree earthSpinScript;
-
-    public string username;
-    public string password;
-    public int RADIUS = 1000;
-    public float ALTITUDE_SCALE_FACTOR = 7.5f;
-    readonly public int EARTH_RADIUS_METRES = 6371000;
-
-    public long lastRequestTime = 0;
-    public long lastPlanePlaceTime = 0;
-    public long lastRequestResponseTime = 0;
-    public long lastSortTime = 0;
-    public string jsonText;
-
-    public Dictionary<string, PlaneData> dataMap = new();
-    public Dictionary<string, GameObject> planeMap = new();
-
+    public int radius = 500;
     // Start is called before the first frame update
     void Start()
     {
 
        
         Camera cam = Camera.main;
-        cam.transform.position = new Vector3(RADIUS * 2, 0, 0);
-        this.earth = CreateEarth(RADIUS);
+        cam.transform.position = new Vector3(radius * 2, 0, 0);
+        this.earth = CreateEarth(radius);
+        earthSpinScript = earth.GetComponent<SpinFree>();
     }
 
 
@@ -52,60 +43,60 @@ public class Gamemanager : MonoBehaviour
             lastRequestTime = DateTimeOffset.Now.ToUnixTimeMilliseconds();
         }
 
-        if (lastRequestResponseTime != lastSortTime) {
-            lastSortTime = lastRequestResponseTime;
-            JSONObject json = JSON.Parse(jsonText) as JSONObject;
-            if (json == null) {
-                return;
-            }
-
-            // Gets the node calle "states" that has all of the plane data
-            JSONNode statesNode = json.GetValueOrDefault("states", null);
-            if (statesNode == null) {
-                return;
-            }
-            JSONArray array = statesNode.AsArray;
-
-            SortData(array);
-
-            
-        }
-
-        AddAllPlanes();
-        UpdateEarth(RADIUS);
-    }
-
-    void AddAllPlanes() {
-        foreach (KeyValuePair<string, PlaneData> pair in dataMap) {
-            if (planeMap.ContainsKey(pair.Key)) {
-                continue;
-            }
-
-            GameObject plane = PlacePlane(pair.Key);
-            planeMap.Add(pair.Key, plane);
+        if (!OnCooldown(lastPlanePlaceTime, 500)) {
+            PlaceAllPlanes(jsonText, radius);
+            lastPlanePlaceTime = DateTimeOffset.Now.ToUnixTimeMilliseconds();
         }
     }
-
-
-    void SortData(JSONArray openskyStatesNodeData) {
-        dataMap.Clear();
-        for (int i = 0; i < openskyStatesNodeData.Count; i++) {
-            string id = openskyStatesNodeData[i].AsArray[0];
-            dataMap.Add(id, new PlaneData(openskyStatesNodeData[i].AsArray));
-        }
+    float GetX(float latitude, float longitude, float radius) {
+        return radius * Mathf.Cos(ToRadians(latitude)) * Mathf.Cos(ToRadians(longitude));
     }
 
+    float GetY(float latitude, float radius) {
+        return radius * Mathf.Sin(ToRadians(latitude));
+    }
+    float GetZ(float latitude, float longitude, float radius) {
+        return radius * Mathf.Cos(ToRadians(latitude)) * Mathf.Sin(ToRadians(longitude));
+    }
 
-    GameObject PlacePlane(string icao24) {
+    // Places planes based off a rest request API from
+    void PlaceAllPlanes(string jsonText, int radius) {
+        JSONObject json = JSON.Parse(jsonText) as JSONObject;
+
+        if (json == null) {
+            return;
+        }
+
+        JSONNode statesNode = json.GetValueOrDefault("states", null);
+
+        if (statesNode == null) {
+            return;
+        }
+
+        JSONArray array = statesNode.AsArray;
+        
+        foreach (GameObject plane in planeObjects) {
+            Destroy(plane);
+        }
+
+        foreach (JSONArray obj in array) {
+            float latitude = obj[6];
+            float longitude = obj[5];
+
+            planeObjects.Add(PlacePlane(latitude, longitude, radius));
+
+        }
+
+    }
+
+    GameObject PlacePlane(float latitude, float longitude, float radius) {
         GameObject point;
+
         point = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        Vector3 position = new(GetX(latitude, longitude, radius), GetY(latitude, radius), GetZ(latitude, longitude, radius));
+        point.transform.position = position;
         MeshRenderer renderer = point.GetComponent<MeshRenderer>();
         renderer.material.color = new Color32(255, 0, 0, 255);
-
-        point.AddComponent<PlaneBehavior>();
-        PlaneBehavior planeBehavior = point.GetComponent<PlaneBehavior>();
-        planeBehavior.Initiate(this, icao24);
-
         return point;
     }
 
@@ -116,14 +107,13 @@ public class Gamemanager : MonoBehaviour
         return earth;
     }
 
-    GameObject UpdateEarth(int radius) {
-        earth.transform.localScale = new Vector3(radius / (40.39f / 2), radius / (40.39f / 2), radius / (40.39f / 2));
-        return earth;
-    }
+    //void PlaceCalibrationPonts() {
+    //    PlacePlane(25.15752503516053f, -80.77371627203654f, radius);
+    //    PlacePlane(28.027522188511185f, -80.56737993613616f, radius);
+    //    PlacePlane(27.88029170652975f, -82.85486971189945f, radius);
+    //}
 
-  
-
-    public static bool OnCooldown(long lastTime, long length) {
+    bool OnCooldown(long lastTime, long length) {
         if (DateTimeOffset.Now.ToUnixTimeMilliseconds() > lastTime + length) {
             return false;
         }
@@ -150,43 +140,11 @@ public class Gamemanager : MonoBehaviour
             else {
                 // Print the response
                 jsonText = webRequest.downloadHandler.text;
-
-                JSONObject json = JSON.Parse(jsonText) as JSONObject;
-                if (json != null) {
-                    lastRequestResponseTime = json.GetValueOrDefault("time", 0).AsInt;
-                }
-
-                
             }
         }
     }
 
-    float AltitudeToPixels(float altitude) {
-        return (altitude * ALTITUDE_SCALE_FACTOR) / (EARTH_RADIUS_METRES / RADIUS);
-    }
-
-    float CalculateX(float latitude, float longitude, float radius) {
-        return radius * Mathf.Cos(ToRadians(latitude)) * Mathf.Cos(ToRadians(longitude));
-    }
-
-    float CalculateY(float latitude, float radius) {
-        return radius * Mathf.Sin(ToRadians(latitude));
-    }
-    float CalculateZ(float latitude, float longitude, float radius) {
-        return radius * Mathf.Cos(ToRadians(latitude)) * Mathf.Sin(ToRadians(longitude));
-    }
-
-    public Vector3 CalculateCoordinates(float latitude, float longitude, float altitude) {
-        return new(CalculateX(latitude, longitude, RADIUS + AltitudeToPixels(altitude)), CalculateY(latitude, RADIUS + AltitudeToPixels(altitude)), CalculateZ(latitude, longitude, RADIUS + AltitudeToPixels(altitude)));
-    }
-
-    public static float ToRadians(float degrees) {
+    float ToRadians(float degrees) {
         return (degrees * Mathf.PI) / 180;
     }
-
-    //void PlaceCalibrationPonts() {
-    //    PlacePlane(25.15752503516053f, -80.77371627203654f, radius);
-    //    PlacePlane(28.027522188511185f, -80.56737993613616f, radius);
-    //    PlacePlane(27.88029170652975f, -82.85486971189945f, radius);
-    //}
 }
